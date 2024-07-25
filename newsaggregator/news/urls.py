4ab352212009b2a86d7210c00666ed87b7671486
Abhraneel Dey <abhradey.2311@gmail.com>
2024-07-26 00:23:23 +0530

    Updated

--- newsaggregator/news/urls.py
+++ newsaggregator/news/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('news.urls')),
+    path('', views.article_list, name='article_list'),
 ]
--- newsaggregator/news/views.py
+++ newsaggregator/news/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Article
 
-# Create your views here.
-def aticle_list(request):
+def article_list(request):
     articles = Article.objects.all()
-    return render(request, 'news/templates/article_list.html', {'articles':articles})
+    return render(request, 'news/article_list.html', {'articles': articles})
