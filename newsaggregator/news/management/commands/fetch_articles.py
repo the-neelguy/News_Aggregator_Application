159f3da419e6411e83cf37d9f6e36e360de5f7e2
Abhraneel Dey <abhradey.2311@gmail.com>
2024-07-26 00:23:55 +0530

    Added Error Handling

--- newsaggregator/news/management/commands/fetch_articles.py
+++ newsaggregator/news/management/commands/fetch_articles.py
@@ -2,6 +2,8 @@ import requests
 from django.core.management.base import BaseCommand
 from news.models import Article
 from datetime import datetime
+from django.utils import timezone
+import pytz
 
 # apikey = 94b4a5d8c5484598ab852fbd28bd95d7
 
@@ -19,13 +21,17 @@ class Command(BaseCommand):
         data = reponse.json()
 
         for item in data['articles']:
+            description = item['description'] if item['description'] is not None else 'Description not available'
+            
+            published_at = datetime.strptime(item['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
+            published_at = timezone.make_aware(published_at, pytz.utc)
+
             article = Article(
                 title = item['title'],
-                content = item['content'],
+                description = description,
                 source = item['source']['name'],
-                published_at = datetime.strptime(item['publishedAt'], '%Y-%m-%dT%H:%M:%SZ'),
+                published_at = published_at,
                 url = item['url']
             )
             article.save()
         self.stdout.write(self.style.SUCCESS('Successfully fetched the articles'))
-        
