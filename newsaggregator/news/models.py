2ad5c2271bdad8b272e1112255d6e2ca3c7ea700
Abhraneel Dey <abhradey.2311@gmail.com>
2024-07-26 00:23:04 +0530

    Changed Content to Description

--- newsaggregator/news/models.py
+++ newsaggregator/news/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    description = models.TextField()
     source = models.CharField(max_length=100)
     published_at = models.DateTimeField()
     url = models.URLField()
