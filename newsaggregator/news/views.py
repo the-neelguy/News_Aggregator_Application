ca701303ba3dd49e8c9577b329a08e111ad9100d
Abhraneel Dey <abhradey.2311@gmail.com>
2024-07-29 00:08:38 +0530

    Error handling for empty search

--- newsaggregator/news/views.py
+++ newsaggregator/news/views.py
@@ -8,7 +8,10 @@ def article_list(request):
 
 def fetch_articles_view(request):
     if 'search_term' in request.GET:
-        search_term = request.GET['search_term']
+        search_term = request.GET['search_term'].strip()
+        if not search_term:
+            articles = Article.objects.all()
+            return render(request, 'news/article_list.html', {'articles': articles, 'error_message': 'Please enter a news topic'})
         call_command('fetch_articles', search_term)
         return redirect('article_list')
-    return render(request, 'news/fetch_articles.html')
+    return render(request, 'news/article_list.html')
