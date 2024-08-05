41a32c7ba9316af2bf2925d162af0f74c0290de7
Abhraneel Dey <abhradey.2311@gmail.com>
2024-08-05 16:59:54 +0530

    Changed the model for max_length

--- newsaggregator/news/models.py
+++ newsaggregator/news/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=300)
     description = models.TextField()
     source = models.CharField(max_length=100)
     published_at = models.DateTimeField()
-    url = models.URLField()
+    url = models.URLField(max_length=300)
 
     def __str__(self):
         return self.title
